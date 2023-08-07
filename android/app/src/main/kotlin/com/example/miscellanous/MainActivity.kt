d2a710d509ada591d98b9440e86036ba62fe722c
Gerardo Rodriguez <garc@outlook.com>
2023-08-06 23:31:45 -0600

    corrected MainActivity.kt file

--- android/app/src/main/kotlin/com/example/miscellanous/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/miscellanous/MainActivity.kt
@@ -1,9 +1,6 @@
 package com.example.miscellanous
 
-import io.flutter.plugins.GeneratedPluginRegistrant
-
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    GeneratedPluginRegistrant.registerWith(this);
 }
